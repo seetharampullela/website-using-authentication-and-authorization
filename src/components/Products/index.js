b5a4fc4f68e7438b63ab7f672595fe45469ad4de
seetharampullela <98651745+seetharampullela@users.noreply.github.com>
2022-03-28 15:57:52 +0530

    change products

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,18 +1,27 @@
 // Write your JS code here
 // Write your JS code here
+import {Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
 import Header from '../Header'
 import './index.css'
 
-const Product = () => (
-  <div>
-    <Header />
-    <div className="div-container">
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
-        alt="products"
-        className="detailed-image"
-      />
+const Product = () => {
+  const jwtToken = Cookies.get('jwt_token')
+
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+  return (
+    <div>
+      <Header />
+      <div className="div-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+          alt="products"
+          className="detailed-image"
+        />
+      </div>
     </div>
-  </div>
-)
+  )
+}
 export default Product
